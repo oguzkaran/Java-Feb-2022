49be158fbb16a102fb9b0e736e29d6c09622c699
Oğuz Karan <oguzkaran@csystem.org>
2022-02-20 10:36:01 +0300

    Update

---
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Bir Java programı genel olarak paketlerden, paketler sınıflardan sınıflardan da metotlardan oluşur.
+	
+	paket bildiriminin genel biçimi:
+	
+	package <isim>;
+	Örneğin 
+	package csd;
+	
+	Paketler konusu ileride detaylı olarak ele alınacaktır
+	
+	Sınıf bildiriminin detayları gözardı edilmiş genel biçimi:
+	
+	class <isim> {
+		//...
+	}
+	
+	Örneğin:
+	class Sample {
+		//...
+	}
+
+	class Mample {
+		//...
+	}
+	
+	Sınıflar konusunun detayları da ileride ele alınacaktır
+	
+	
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+	public static void main(String [] args)
+	{		
+		System.out.println("Hello, World");
+	}
+}
+
+
+class Sample {
+	//...
+}
+
+class Mample {
+	//...
+}
