ca61bef46a24e1aaf5e8f93267d3dc407ebe601f
Oğuz Karan <oguzkaran@csystem.org>
2022-02-26 12:48:17 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,28 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Java'da 3(üç) çeşit değişken vardır:
-	1. Yerel değişkenler (local variables)
-	2. Parametre değişkenleri (parameter variables)
-	3. Sınıf veri elemanları (class member variables)
-	
-	Değişkenler için temel kavramlar:
-	İsim (name): Belli kurallara göre yazılan karakterler topluluğudur.
-	
-	Tür (type): Değişken için bellekte ne kadar yer ayrılacağı ve içerisinde değeriln hagi yönteme göre tutulacağını
-	belirtir.
-	
-	Faaliyet alanı (scope): Bir değişken isminin derleyici tarafından görülebildiği kod aralığıdır. Derleme zamanına
-	ilişkindir
-	
-	Ömür (storage duration): Bir değişkenin bellekte kalma süresidir. Çalışma zamanına ilişkindir. 
-	
-	
+	Java 9 ile birlikte alttire karakterinin tek başına değişken ismi olarak kullanılması geçersizdir. Java 8 ile birlikte
+	"deprecated" olmuştur
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{		
-						
+	{			
+		
 	}
 }
-
