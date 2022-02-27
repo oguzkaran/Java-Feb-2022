7150001a418837e9d6ca703f618bb132d0fcfa8e
Oğuz Karan <oguzkaran@csystem.org>
2022-02-27 12:48:48 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,12 +1,30 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Java 9 ile birlikte alttire karakterinin tek başına değişken ismi olarak kullanılması geçersizdir. Java 8 ile birlikte
-	"deprecated" olmuştur
+	void metotlar geri dönüş değeri varmış gibi çağrılamaz. Yani void metot çağrısı geri dönüş varmış gibi işleme sokulamaz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
-class App {
-	public static void main(String [] args)
-	{			
-		
+class App {	
+	public static void main(String [] args) 
+	{		
+		NumberUtil.printAdd();
 	}
 }
+
+class NumberUtil {
+	public static void printAdd()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Birinci sayıyı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());
+		
+		System.out.print("İkinci sayıyı giriniz:");
+		int b = Integer.parseInt(kb.nextLine());		
+		int result;
+		
+		result = a + b;		
+		
+		System.out.println(result);
+	}
+}
+
