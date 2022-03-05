e837f2f2536d8ee9c33977a71e409c20102df644
Oğuz Karan <oguzkaran@csystem.org>
2022-03-05 10:41:50 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,30 +1,30 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	void metotlar geri dönüş değeri varmış gibi çağrılamaz. Yani void metot çağrısı geri dönüş varmış gibi işleme sokulamaz
+/*----------------------------------------------------------------------------------------------------------------------,
+	Yukarıdaki kod aşağıdaki gibi bir çözümle daha iyi hale getirilebilir	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args) 
 	{		
-		NumberUtil.printAdd();
-	}
-}
-
-class NumberUtil {
-	public static void printAdd()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
 		System.out.print("Birinci sayıyı giriniz:");
 		int a = Integer.parseInt(kb.nextLine());
 		
 		System.out.print("İkinci sayıyı giriniz:");
 		int b = Integer.parseInt(kb.nextLine());		
-		int result;
 		
-		result = a + b;		
+		System.out.println(NumberUtil.add(a, b));
 		
-		System.out.println(result);
+		//...
+		
+		System.out.println(NumberUtil.add(a, b));			
+	}
+}
+
+class NumberUtil {
+	public static int add(int a, int b)	
+	{		
+		return a + b;		
 	}
 }
 
