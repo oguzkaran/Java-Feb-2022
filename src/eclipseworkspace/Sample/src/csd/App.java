4a6f9f17f24cca23e49626273a91005d26fde118
Oğuz Karan <oguzkaran@csystem.org>
2022-03-06 10:46:28 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,15 +1,29 @@
 /*----------------------------------------------------------------------------------------------------------------------,
-	printf metodunda uygun format karakterleri kullanılmadığında genel olarak exception oluşur
+	Neden standart metotları kullanmalıyız? Yani standart metodu yazmalı mıyız?
+	Java'da standart olarak (JavaSE) bulunan metotların kullanılmasının bazı önemli avantajları:
+	- Okunabilir kodlar yazılır
+	- Testleri yapılmıştır ve yeterince erkin olarak yazılmıştır
+	- Taşınabilirdir
+	- Bazı metotlar (ki bunların sayısı oldukça fazladır) başka detay konuları da bilmeyi gerektirir
+	- Programcının projede sadece kendi konusuna odaklanmasını sağlar
+	
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
-	{		
-		int a = 10;
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Birinci sayıyı giriniz:");
+		double a = Double.parseDouble(kb.nextLine());
+		System.out.print("İkinci sayıyı giriniz:");
+		double b = Double.parseDouble(kb.nextLine());
 		
-		System.out.printf("a = %b%n", a);
-		System.out.println("Tekrar yapıyor musunuz?");
+		double result;
+		result = a / b;
+		
+		System.out.printf("%f / %f = %f%n", a, b, result);
 	}
 }
 
