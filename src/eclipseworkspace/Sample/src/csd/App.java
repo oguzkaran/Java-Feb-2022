07c4bfcf6c8d10444e8ea9fd24fc3fca9fd40207
Oğuz Karan <oguzkaran@csystem.org>
2022-03-12 11:58:32 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,38 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------,
-	Math sınıfının min ve max metotları ile 3 tane sayının en küçüğünün ve en büyüğünün bulunması
+	Carriage return karakter sabiti 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Birinci sayıyı giriniz:");
-		int a = Integer.parseInt(kb.nextLine());
+		char c = '\t';
 		
-		System.out.print("İkinci sayıyı giriniz:");
-		int b = Integer.parseInt(kb.nextLine());
-		
-		System.out.print("Üçüncü sayıyı giriniz:");
-		int c = Integer.parseInt(kb.nextLine());
-		
-		System.out.printf("min(%d, %d, %d) = %d%n", a, b, c, MathUtil.min(a, b, c));
-		System.out.printf("max(%d, %d, %d) = %d%n", a, b, c, MathUtil.max(a, b, c));
+		System.out.print("Merhaba");
+		System.out.print(c);
+		System.out.print("Test");
 	}
-}
-
-class MathUtil {
-	public static int min(int a, int b, int c)
-	{
-		return Math.min(Math.min(a, b), c);
-		
-	}
-	
-	public static int max(int a, int b, int c)
-	{
-		return Math.max(Math.max(a, b), c);
-		
-	}
-	
-	//...
 }
