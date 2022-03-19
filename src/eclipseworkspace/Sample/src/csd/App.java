caa7c27f2c27ff7cc205e3205842c7f933e0d849
Oğuz Karan <oguzkaran@csystem.org>
2022-03-19 12:34:07 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,11 +1,27 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Aşağıdaki örnekte ++ ve -- operatörleri ve operandları aynı ifadede kullanılmıştır
+	Mantıksal Operatörler 3 tanedir: && (AND), || (OR), ! (NOT)
+	&& ve || operatörleri iki operandlı araek durumunda operatörlerdir. ! operatörü tek operandlı önek durumunda bir
+	operatördür. Bu üç operatörün de operandları boolean türden olmalıdır. Bu üç operatörün ürettiği değer de boolean
+	türdendir. Bu operatörler Matematik'teki mantıksal operatörlere karşılık gelir.	
+	
+	&& ve || operatörlerinin doğruluk tablosu aşağıdaki gibidir:
+	
+	op1			op2		op1 && op2		op1 || op2
+	T			T			T				T
+	T			F			F				T
+	F			T			F				T
+	F			F			F				F
+	
+	Yukarıdaki tabloya göre şu cümleler söylenebilir:
+	"Mantıksal AND işlemi için, en az biri yanlışsa sonuç yanlıştır"
+	"Mantıksal OR işlemi için, en az biri doğruysa sonuç doğrudur"
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{
-		
+						
 	}
 }
+
