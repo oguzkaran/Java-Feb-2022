0fd1928a6c92eac7814eafbf37e2208984ae40c9
Oğuz Karan <oguzkaran@csystem.org>
2022-03-26 10:29:27 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,12 +1,24 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Yukarıdaki "dangling else" durumu bileşik deyim kullanılarak çözülebilir
+	Aşağıdaki koşullar ayrık olmadığında örnek özelinde else-if biçiminde kullanılması yanlış olur
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Bir sayı giriniz:");
+		int val = Integer.parseInt(kb.nextLine());
 		
+		if (val > 6)
+			System.out.println("val > 6");
+		if (val > 11)
+			System.out.println("val > 11");
+		if (val > 21)
+			System.out.println("val > 21");
 		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
+
+
