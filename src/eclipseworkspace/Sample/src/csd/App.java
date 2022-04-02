638a1337f64f4135be54a0f3a6d7689a3e8d1bcd
Oğuz Karan <oguzkaran@csystem.org>
2022-04-02 12:00:47 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,10 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	 Sınıf Çalışması: Parametresi ile aldığı int türden bir n değeri için n-inci asal sayıyı döndüren getPrime isimli
-	 metodu yazınız ve aşağıdaki kod ile test ediniz.
+	 Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının faktoriyel değerini döndüren factorial isimli metodu
+	 NumberUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
+	 Açıklamalar:
+	 - Faktoriyel işlemi aşağıdaki gibidir: 
+		 0! = 1
+		 1!= 1
+		 2! = 1 * 2
+		 3! = 1 * 2 * 3
+		 
+		 ...
+		 
+		 n! = 1 * 2 * 3 * ... * (n - 1) * n
 	 
-	 Açıklamalar: 
-	 	- isPrime metodunun hızlı versiyonunu hızlı olduğu bilinciyle getPrime metodunu yazınız
-	 	- n değerinin pozitif olmaması durumu metot içerisinde kontrol edilmeyecektir
+	 - Negatif sayılar için faktoriyel değeri 1 olarak döndürelecektir	 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -18,51 +26,16 @@ class App {
 class GetPrimeTest {
 	public static void run()
 	{		
-		java.util.Scanner kb = new java.util.Scanner(System.in);		
-		
-		for (;;) {
-			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());		
-			
-			
-			if (n <= 0) {
-				System.out.println("Tekrar yapıyor musunuz?");
-				return;
-			}
-			
-			System.out.printf("%d. asal sayı:%d%n", n, NumberUtil.getPrime(n));
-		}		
+		for (int n = -1; n <= 12; ++n)
+			System.out.printf("%d! = %d%n", n, NumberUtil.factorial(n));
 	}
 }
 
 class NumberUtil {
-	public static int getPrime(int n)
+	public static int factorial(int n)
 	{
 		//TODO:
-	}
+	}	
 	
-	public static boolean isPrime(long a)
-	{
-		if (a <= 1)
-			return false;
-		
-		if (a % 2 == 0)
-			return a == 2;
-		
-		if (a % 3 == 0)
-			return a == 3;		
-		
-		if (a % 5 == 0)
-			return a == 5;
-		
-		if (a % 7 == 0)
-			return a == 7;
-
-		for (long i = 11; i * i <= a; i += 2)
-			if (a % i == 0)
-				return false;
-		
-		return true;		
-	}
 }
 
