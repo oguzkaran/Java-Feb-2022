9e68fc533b890f8dafb8f4799ea7a4e7411ae528
Oğuz Karan <oguzkaran@csystem.org>
2022-04-09 11:12:30 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,33 +1,29 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Parametresi ile aldığı n sayısı için n-inci Fibonacci sayısını döndüren fibonacciNumber isimli 
-	metodu NumberUtil sınıfı içerisinde ve aşağıdaki kod ile test ediniz
-	Açıklama:
-		- Fibonnaci sayıları
-			0 1 1 2 3 5 8 13 21 34 ...
-		- Metot pozitif olmayan n değerleri için kontrol yapmayacaktır
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayıdan büyük ilk Fibonacci sayısına geri dönen nextFibonacciNumber
+	isimli metodu yazınız ve aşağıdaki kod ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{		
-		FibonacciNumberTest.run();		
+		NextFibonacciNumberTest.run();		
 	}
 }
 
-class FibonacciNumberTest {
+class NextFibonacciNumberTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
 			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
+			int val = Integer.parseInt(kb.nextLine());
 			
-			if (n < 1)
+			if (val < -999)
 				break;
 			
-			System.out.printf("%d. Fibonacci sayısı:%d%n", n, NumberUtil.fibonacciNumber(n));
+			System.out.printf("%d. sayısından büyük ilk Fibonacci sayısı:%d%n", val, NumberUtil.nextFibonacciNumber(val));
 		}
 		
 		System.out.println("Tekrar yapıyor musunuz?");
@@ -35,6 +31,11 @@ class FibonacciNumberTest {
 }
 
 class NumberUtil {
+	public static int nextFibonacciNumber(int n) 
+	{
+		//TODO:
+	}
+	
 	public static int fibonacciNumber(int n) 
 	{
 		if (n <= 2)
