9b5ac169927de762161a13472d952113b62326e3
Oğuz Karan <oguzkaran@csystem.org>
2022-04-16 10:31:52 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,93 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Programcı ne zaman switch expression, ne zaman switch statement kullanmalıdır? Şüphesiz programcı Java 12 öncesinde
-	çalışıyorsa switch expression kullanamaz. Bu durumda programcı zorunlu olmadıkça, okunabilirlik açısından bir sorun da yoksa
-	switch expression tercih etmelidir
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
-	{		
-		MenuApp.run();
-	}
-}
-
-class MenuApp {
-	public static void run()
-	{
-		Menu.run();
-	}
-}
-
-class Menu {
-	public static void printMenu() 
-	{
-		System.out.println("1.Ekle");
-		System.out.println("2.Güncelle");
-		System.out.println("3.Sil");
-		System.out.println("4.Listele");
-		System.out.println("5. Çıkış");
-		System.out.print("Seçenek:");
-	}
-	
-	public static void doWorkForInsert()
-	{
-		System.out.println("------------------------------------");
-		System.out.println("\"Ekle\" seçildi");
-		System.out.println("------------------------------------");
-	}
-	
-	public static void doWorkForUpdate()
-	{
-		System.out.println("------------------------------------");
-		System.out.println("\"Güncelle\" seçildi");
-		System.out.println("------------------------------------");
-	}
-	
-	public static void doWorkForDelete()
-	{
-		System.out.println("------------------------------------");
-		System.out.println("\"Sil\" seçildi");
-		System.out.println("------------------------------------");
-	}
-	
-	public static void doWorkForList()
-	{
-		System.out.println("------------------------------------");
-		System.out.println("\"Listele\" seçildi");
-		System.out.println("------------------------------------");
-	}
-	
-	public static void doWorkForQuit()
-	{
-		System.out.println("------------------------------------");
-		System.out.println("Teşekkürler");
-		System.out.println("C ve Sistem Programcıları Derneği");
-		System.out.println("Tekrar yapıyor musunuz?");
-		System.out.println("------------------------------------");
-		System.exit(0);
-	}
-	
-	
-	public static void doWorkFor(int option)
-	{
-		switch (option) {
-		case 1 -> doWorkForInsert();			
-		case 2 -> doWorkForUpdate();
-		case 3 -> doWorkForDelete();			
-		case 4 -> doWorkForList();			
-		case 5 -> doWorkForQuit();
-		default -> System.out.println("\"Geçersiz seçenek\"");
-		}		
-	}
-	
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
+	{	
+		float a = 12.4F;
 		
-		for (;;) {
-			printMenu();			
-			doWorkFor(Integer.parseInt(kb.nextLine()));
-		}		
+		System.out.printf("a = %.12f%n", a);
 	}
 }
+
