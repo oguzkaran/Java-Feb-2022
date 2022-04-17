30bc39914d471e4071425c8aac9fe0f1e61512ac
Oğuz Karan <oguzkaran@csystem.org>
2022-04-17 12:28:00 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,20 +1,52 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Gerçek sayı türünde tamsayı türüne  
+	 Koşul Operatörü (conditional operator):	 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
-	{		
-		double a = -5_000_000_000.89;
-		short b;
-		
-		b = (short)a;
-		
-		System.out.printf("-5_000_000_000 = %08X%n", (int)-5_000_000_000L);
-		System.out.printf("-2147483648 = %d, %08X%n", -2147483648, -2147483648);
-		System.out.printf("b = %d, %X%n", b, b);			
-		
+	{
+		IsArmstrongTest.run();
 	}
 }
+
+class IsArmstrongTest {
+	public static void run()
+	{
+		for (int a = -10; a <= 999999; ++a)
+			if (NumberUtil.isArmstrong(a))
+				System.out.println(a);
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}
+}
+
+class NumberUtil {
+	public static boolean isArmstrong(int a)
+	{
+		return a >= 0 && getDigitsPowSum(a) == a;				
+	}
+	
+	public static int getDigitsPowSum(int a)
+	{
+		int n = countDigits(a);
+		int total = 0;
+		
+		while (a != 0) {
+			total += Math.pow(a % 10, n);
+			a /= 10;					
+		}
+		
+		return total;
+	}	
+	
+	public static int countDigits(int a)
+	{
+		if (a == 0)
+			return 1;
+		
+		return (int)Math.log10(Math.abs(a)) + 1;
+	}
+}
+
 
