09be20cbad2a37218f6105f9c40b149d52d25c79
Oğuz Karan <oguzkaran@csystem.org>
2022-04-24 12:54:50 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,42 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	 
+	Değişkenlerin Ömürleri:
+	Bir değişkenin ömrü onun bellekte yer kapladığı zaman aralığıdır. Yani bir değişkenin ömrü o değişkenin bellekte
+	yaratılmasıyla (yer ayrılmasıyla) 	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{	
-		
+				
 	}
 }
 
-class MyMath {	
-	public static int abs(int a) //#1
-	{
-		System.out.println("abs, int");
-		
-		return a < 0 ? -a : a; 
-	}
-	
-	public static double abs(double a)  //#2
-	{
-		System.out.println("abs, double");
-		
-		return a < 0 ? -a : a; 
-	}
-	
-	public static float abs(float a)  //#3
-	{
-		System.out.println("abs, float");
-		
-		return a < 0 ? -a : a; 
-	}
-	
-	
-	public static long abs(long a)  //#4
-	{
-		System.out.println("abs, long");
-		
-		return a < 0 ? -a : a; 
-	}
-}
