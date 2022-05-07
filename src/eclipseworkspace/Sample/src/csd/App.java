c1b55a3f966c31a889f31d1119d3d54b143d8033
Oğuz Karan <oguzkaran@csystem.org>
2022-05-07 14:37:41 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,35 +1,59 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	 Sınıf türünden bir değişkene (aslında referans türünden bir değişkene) "referans değişken" ya da kısaca "referans"
-	 denir. 
-	 
-	 Anahtar Notlar: Java'da "referans" sözcüğü "adres" anlamında da kullanılır. Örneğin "bir nesnenin adresi" yerine
-	 "bir nesnenin referansı" demek de aynıdır. Ancak buradaki referans terimi bir değişkeni değil adresi temsil eder.
-	 "Referans değişken ya da kısaca referans" içerisinde adres (referans) tutan değişkendir.     
+	Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{	
-		Sensor s; //s bir referans (değişken)
-		Student st; //st bir referans
-		Person p; //p bir referans
+		Sample s, k;	
 		
-		//...						
+		s = new Sample();
+		k = new Sample();		
+		
+		s.a = 20;
+		s.b = true;
+		
+		k.a = 30; 
+		
+		System.out.printf("s.a = %d%n", s.a);
+		System.out.printf("s.b = %b%n", s.b);
+		System.out.printf("k.a = %d%n", k.a);
+		System.out.printf("k.b = %b%n", k.b);
+		System.out.println("------------------------------------");
+		
+		s = k; //**
+		
+		System.out.printf("s.a = %d%n", s.a);
+		System.out.printf("s.b = %b%n", s.b);
+		System.out.printf("k.a = %d%n", k.a);
+		System.out.printf("k.b = %b%n", k.b);
+		System.out.println("------------------------------------");
+		
+		++s.a;
+		
+		System.out.printf("s.a = %d%n", s.a);
+		System.out.printf("s.b = %b%n", s.b);
+		System.out.printf("k.a = %d%n", k.a);
+		System.out.printf("k.b = %b%n", k.b);
+		System.out.println("------------------------------------");
+		
+		s = new Sample();
+		
+		--s.a;
+		
+		System.out.printf("s.a = %d%n", s.a);
+		System.out.printf("s.b = %b%n", s.b);
+		System.out.printf("k.a = %d%n", k.a);
+		System.out.printf("k.b = %b%n", k.b);
+		System.out.println("------------------------------------");
 	}
 }
 
 
-class Sensor {
+class Sample {
+	public int a;
+	public boolean b;
+	
 	//...
 }
-
-class Student {
-	//...
-}
-
-class Person {
-	//...
-}
-
-
