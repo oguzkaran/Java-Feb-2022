1db8c22355ab3ea731d5cf072c2beb205d4c0faa
Oğuz Karan <oguzkaran@csystem.org>
2022-05-07 10:31:08 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,14 +1,35 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Değişkenlerin Ömürleri:
-	Bir değişkenin ömrü onun bellekte yer kapladığı zaman aralığıdır. Yani bir değişkenin ömrü o değişkenin bellekte
-	yaratılmasıyla (yer ayrılmasıyla) 	
+	 Sınıf türünden bir değişkene (aslında referans türünden bir değişkene) "referans değişken" ya da kısaca "referans"
+	 denir. 
+	 
+	 Anahtar Notlar: Java'da "referans" sözcüğü "adres" anlamında da kullanılır. Örneğin "bir nesnenin adresi" yerine
+	 "bir nesnenin referansı" demek de aynıdır. Ancak buradaki referans terimi bir değişkeni değil adresi temsil eder.
+	 "Referans değişken ya da kısaca referans" içerisinde adres (referans) tutan değişkendir.     
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{	
-				
+		Sensor s; //s bir referans (değişken)
+		Student st; //st bir referans
+		Person p; //p bir referans
+		
+		//...						
 	}
 }
+
+
+class Sensor {
+	//...
+}
+
+class Student {
+	//...
+}
+
+class Person {
+	//...
+}
+
 
