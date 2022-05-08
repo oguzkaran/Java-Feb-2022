04822aa802f562921f90b54f793733541ca0d666
Oğuz Karan <oguzkaran@csystem.org>
2022-05-08 10:29:37 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,59 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Aşağıdaki örneği inceleyiniz
+	Sınıfın non-static bir metodu sınıf dışından sınıf ismi ve nokta operatörü ile çağrılamaz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
-	{	
-		Sample s, k;	
-		
-		s = new Sample();
-		k = new Sample();		
-		
-		s.a = 20;
-		s.b = true;
-		
-		k.a = 30; 
-		
-		System.out.printf("s.a = %d%n", s.a);
-		System.out.printf("s.b = %b%n", s.b);
-		System.out.printf("k.a = %d%n", k.a);
-		System.out.printf("k.b = %b%n", k.b);
-		System.out.println("------------------------------------");
-		
-		s = k; //**
-		
-		System.out.printf("s.a = %d%n", s.a);
-		System.out.printf("s.b = %b%n", s.b);
-		System.out.printf("k.a = %d%n", k.a);
-		System.out.printf("k.b = %b%n", k.b);
-		System.out.println("------------------------------------");
-		
-		++s.a;
-		
-		System.out.printf("s.a = %d%n", s.a);
-		System.out.printf("s.b = %b%n", s.b);
-		System.out.printf("k.a = %d%n", k.a);
-		System.out.printf("k.b = %b%n", k.b);
-		System.out.println("------------------------------------");
-		
-		s = new Sample();
-		
-		--s.a;
-		
-		System.out.printf("s.a = %d%n", s.a);
-		System.out.printf("s.b = %b%n", s.b);
-		System.out.printf("k.a = %d%n", k.a);
-		System.out.printf("k.b = %b%n", k.b);
-		System.out.println("------------------------------------");
+	{		
+		Sample.foo(); //error
 	}
 }
 
-
 class Sample {
-	public int a;
-	public boolean b;
-	
-	//...
+	public void foo()
+	{
+		//...
+	}
 }
