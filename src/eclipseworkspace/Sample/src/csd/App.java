ad6f8e94f3208d4919d21f7eaba4bb346dd25788
Oğuz Karan <oguzkaran@csystem.org>
2022-05-28 10:28:29 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,145 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	
+	Derleyici özdeş string sabitleri (string literals) için aynı nesnenin referansını verecek bir kod üretir. Yani bir
+	string sabiti ile özdeş kaç tane sabit olursa olsun hepsi için aynı nesnenin referansı elde edilir. Yeniden nesne
+	yaratılmaz. Aşağıdaki örnekte yine referans karşılaştırması yapılmıştır. Ancak s1 ve s2'ye atanan string'ler özdeş
+	olduğundan aynı adresler tutulmuş olur. Örnek durumu anlatmak için yazılmıştır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
-	{
-		Complex z = new Complex(3, 5);
+	{		
+		String s1 = "ankara";		
+		String s2 = "ankara";
 		
-		z.inc(3);		
-		z.print();
-		z.dec(3);
-		z.print();		
+		System.out.println(s1 == s2 ? "Aynı yazı" : "Farklı yazılar");
 	}
 }
 
-class Complex {
-	public double real;
-	public double imag;
-	
-	public static Complex add(double a1, double b1, double a2, double b2) //İleride bu metot gizlenecektir
-	{		
-		return new Complex(a1 + a2, b1 + b2);
-	}
-	
-	public static Complex subtract(double a1, double b1, double a2, double b2) //İleride bu metot gizlenecektir
-	{		
-		return add(a1, b1, -a2, -b2);	
-	}
-	
-	public static Complex multiply(double a1, double b1, double a2, double b2) //İleride bu metot gizlenecektir
-	{		
-		return new Complex(a1 * a2 - b1  * b2, a1 * b2 + a2 * b1);	
-	}
-	
-	public Complex()
-	{		
-	}
-	
-	public Complex(double re)
-	{
-		real = re;		
-	}
-	
-	public Complex(double re, double im)
-	{
-		real = re;
-		imag = im;
-	}
-	
-	public double getNorm()
-	{
-		return Math.sqrt(real * real + imag * imag);
-	}
-	
-	public double getLength()
-	{
-		return getNorm();
-	}
-	
-	
-	public Complex getConjugate()
-	{
-		return new Complex(real, -imag);
-	}
-	
-	//add	
-	public static Complex add(double value, Complex z)
-	{
-		return add(value, 0, z.real, z.imag);		
-	}
-	
-	public Complex add(Complex other)
-	{
-		return add(real, imag, other.real, other.imag);
-	}
-	
-	public Complex add(double value)
-	{
-		return add(real, imag, value, 0);
-	}
-	
-	//subtract
-	public static Complex subtract(double value, Complex z)
-	{
-		return subtract(value, 0, z.real, z.imag);		
-	}
-	
-	public Complex subtract(Complex other)
-	{
-		return subtract(real, imag, other.real, other.imag);
-	}
-	
-	public Complex subtract(double value)
-	{
-		return subtract(real, imag, value, 0);
-	}
-	
-	
-	//multiply
-	public static Complex multiply(double value, Complex z)
-	{
-		return multiply(value, 0, z.real, z.imag);		
-	}
-	
-	public Complex multiply(Complex other)
-	{
-		return multiply(real, imag, other.real, other.imag);
-	}
-	
-	public Complex multiply(double value)
-	{
-		return multiply(real, imag, value, 0);
-	}
-	
-	//divide (TODO)
-	
-	//inc
-	public void inc(double value)
-	{
-		real += value;
-	}
-	
-	public void inc()
-	{
-		inc(1);
-	}
-	
-	//dec
-	public void dec(double value)
-	{
-		inc(-value);		
-	}
-	
-	public void dec()
-	{
-		dec(1);
-	}	
-	
-	public void print()
-	{
-		System.out.printf("|%.2f, %.2f| = %f%n", real, imag, getNorm());
-	}
-}
