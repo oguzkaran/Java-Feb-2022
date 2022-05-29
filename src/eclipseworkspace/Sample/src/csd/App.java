eb2a17d6ca55d057b47ded81b30f4000af2cf6c1
Oğuz Karan <oguzkaran@csystem.org>
2022-05-29 10:37:51 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,40 +1,65 @@
-/*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının tersini döndüren reverse isimli metodu StringUtil sınıfı
-	içerisinde yazınız ve aşağıdaki kod ile test ediniz
-----------------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------------
+    IntegerDecimalOccurence 
+	String pattern matching in decimal representations of ints.
+	
+	Task description
+	Two integers A and B are given. We are interested in positions at which decimal representation of A occurs as a substring 
+	in the decimal respreesentation of B (counting from 0) For example:
+	
+		- 53 occurs in 1953786 at position 2.
+		- 78 occurs in 1953786786 at positions 4 and 7
+		- 57 does not occur in 153786.
+	
+	Decimal representations are assumed to be big-endian and without leading zeros (the only exception being the number 0, whose 
+	decimal representation is "0")
+	
+	Write function
+		
+		class Solution { public int solution(int A, int B); }
+	
+	that, given two integers A and B, returns the leftmost position at which A occurs in B the function should return -1 
+	if A does not occur in B. 
+	For excample, given A = 53 and B = 1953786, the function should return 2, as explained above.
+	Assume thet:
+		
+		- A and B are integers within the range [0..999,999,999].
+		
+	In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{		
-		ReverseTest.run();		
+		ConcatWithHyphenApp.run();	
 	}
 }
 
-class ReverseTest {
+class ConcatWithHyphenApp {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.println("Yazıları girmeye başlayınız:");
+		
+		String str = "";		
+		
 		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String str = kb.nextLine();
+			String s = kb.nextLine();
 			
-			if ("elma".equals(str))
+			if (s.trim().isEmpty())
+				continue;			
+			
+			if ("elma".equals(s))				
 				break;
 			
-			System.out.printf("Yazının tersi:%s%n", StringUtil.reverse(str));
-		}
+			if (!str.isEmpty())				
+				str += "-";
+			
+			str += s;
+		}	
 		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-
-
-class StringUtil {
-	public static String reverse(String str)
-	{
-		//TODO
+			
+		System.out.println(str);
 	}
 }
