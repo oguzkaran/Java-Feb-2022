6a0860eb00c4ae3ff1292bec475d8457ecac94d4
Oğuz Karan <oguzkaran@csystem.org>
2022-05-29 09:43:07 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,85 +1,40 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Yukarıdaki uygulamanın kullanıcı adını karttan okuyan sadece şifre isteyen versiyonu
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının tersini döndüren reverse isimli metodu StringUtil sınıfı
+	içerisinde yazınız ve aşağıdaki kod ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{		
-		AtmApp.run(3);
+		ReverseTest.run();		
 	}
 }
 
-class AtmApp {
-	
-	public static void doSuccess(AtmCard atmCard, java.util.Scanner kb)
-	{
-		System.out.println("-------------------------");
-		System.out.println("Giriş başarılı");
-		System.out.println("-------------------------");
-		//...
-	}
-	
-	public static void doFail(AtmCard atmCard, java.util.Scanner kb)
-	{
-		System.out.println("-------------------------");
-		System.out.println("Giriş başarısız. Artık deneme hakkınız biti!...");
-		System.out.println("-------------------------");
-		//...
-	}
-	
-	public static void doWorkForCard(AtmCard atmCard, int tryNum, java.util.Scanner kb)
-	{
-		int i = 0;
-		
-		for (; i < tryNum; ++i) {			
-			System.out.print("Şifreyi giriniz:");
-			String password = kb.nextLine();
-			
-			if (atmCard.isvalid(password))
-				break;
-			
-			if (i != tryNum - 1)
-				System.out.printf("Giriş başarısız. %d giriş hakkınız kaldı!...%n", tryNum - 1 - i);
-		}
-		
-		if (i != tryNum)
-			doSuccess(atmCard, kb);
-		else
-			doFail(atmCard, kb);		
-	}
-	
-	public static void run(int tryNum)
+class ReverseTest {
+	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		for (;;) {			
-			AtmCard atmCard = new AtmCard(); //Her defasında yeni kart girişi yapılıyor olarak düşününüz
-			System.out.printf("Hoş geldiniz %s%n", atmCard.name);
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String str = kb.nextLine();
 			
-			doWorkForCard(atmCard, tryNum, kb);			
+			if ("elma".equals(str))
+				break;
+			
+			System.out.printf("Yazının tersi:%s%n", StringUtil.reverse(str));
 		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
-class AtmCard {
-	public String name;
-	public String password;
-	
-	public AtmCard() 
+
+
+class StringUtil {
+	public static String reverse(String str)
 	{
-		//Burada kartın içinden okunuyor olarak düşününüz
-		name = "Ali Vefa Serçe";
-		password = "1993";
+		//TODO
 	}
-	
-	public boolean isvalid(String passwd)
-	{
-		return passwd.equals(password);
-	}
-	
-	//...
 }
-
-
-
