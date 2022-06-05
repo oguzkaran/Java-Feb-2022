2145b683ba2c8b0068799586249f26c38aa1c5a5
Oğuz Karan <oguzkaran@csystem.org>
2022-06-05 10:39:33 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,46 +1,17 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının palindrom olup olmadığını test eden isPalindrome metodunu
-	yazınız ve aşağıdaki kod ile test ediniz.
-	Palindrom: Yalnızca alfabetik karakterleri tersten okunduğunda aynı olan yazılara palindrom denir. Detayların gözardı
-	edilmesi koşuluyla bu tanım yazılabilir.
-	
-	Örnek:
-	Ey Edip Adana'da pide ye!	-> eyedipadanadapideye
-	Anastas mum satsana			-> anastasmumsatsana
-	Ali Papila 					-> alipapila	  		
+	String sınıfının valueOf metotları ile temel türlerin yazı karşılığı elde edilebilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {	
 	public static void main(String [] args)
 	{
-		IsPalindromeTest.run();
-	}
-}
-
-class IsPalindromeTest {
-	public static void run()
-	{		
-		java.util.Scanner kb = new java.util.Scanner(System.in);		
+		int a = 10;		
+		String s = String.valueOf(a);
 		
-		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			if ("elma".equals(s))
-				break;
-			
-			System.out.println(StringUtil.isPalindrome(s) ? "Palindrom" : "Palindrom değil");
-		}				
 		
-		System.out.println("Tekrar yapıyor musunuz?");
+		System.out.println(s);				
 	}
 }
 
 
-class StringUtil {	
-	public static boolean isPalindrome(String s)
-	{
-		//TODO:
-	}
-}
