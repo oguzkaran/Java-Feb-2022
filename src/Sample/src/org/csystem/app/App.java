e80c64b45d956f234b70e8b737d385a8f52ddfc7
Oğuz Karan <oguzkaran@csystem.org>
2022-06-18 11:29:03 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir dizinin elemanlarının toplamına geri dönen sum misimli metodu
-	ArrayUtil sınıfı içersinde yazınız ve aşağıdaki kod ile test ediniz
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir dizinin elemanları ters yüz eden reverse isimli metodu
+	başka bir dizi kullanmadan yazınız ve aşağıdaki kod ile tes ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -11,21 +11,15 @@ import static org.csystem.util.array.ArrayUtil.*;
 class App {
 	public static void main(String [] args)
 	{
-		SumIntArrayTest.run();
+		ReverseArrayTest.run();
 	}	
 }
 
-class SumIntArrayTest {
+class ReverseArrayTest {
 	public static void run()
 	{
 		Scanner kb = new Scanner(System.in);
 		Random r = new Random();
-
-		System.out.print("Birinci sayıyı giriniz:");
-		int a = kb.nextInt();
-
-		System.out.print("İkinci sayıyı giriniz:");
-		int b = kb.nextInt();
 
 		for (;;) {
 			System.out.print("Dizinin eleman sayısını giriniz:");
@@ -34,10 +28,11 @@ class SumIntArrayTest {
 			if (count <= 0)
 				break;
 
-			int [] numbers = new int[count];
+			int [] a = getRandomArray(r, count, 0, 99);
 
-			print(numbers);
-			System.out.printf("Toplam:%d%n", sum(numbers));
+			print(a);
+			reverse(a);
+			print(a);
 		}
 
 		System.out.println("Tekrar yapıyor musunuz?");
--- src/Sample/src/org/csystem/util/array/ArrayUtil.java
+++ src/Sample/src/org/csystem/util/array/ArrayUtil.java
@@ -36,10 +36,21 @@ public class ArrayUtil {
         System.out.println();
     }
 
-    public static int sum(int [] a)
+    public static void reverse(int [] a)
     {
         //TODO:
     }
+
+    public static int sum(int [] a)
+    {
+        int total = 0;
+
+        for (int i = 0; i < a.length; ++i)
+            total += a[i];
+
+        return total;
+    }
+
     public static void swap(int [] a, int i, int k)
     {
         int temp =  a[i];
