3cbcbdc345f085532dd351a287a22d6c30d15e7c
Oğuz Karan <oguzkaran@csystem.org>
2022-07-17 10:28:09 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,35 +1,66 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıfın no-modifier bölümü aynı paketteki diğer sınıflar için public anlamındadır (friendly)
+
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
+		Date birthDate = new Date(11, 7, 1983);
 
+		System.out.printf("%02d/%02d/%04d%n", birthDate.getDay(), birthDate.getMonth(), birthDate.getYear());
+
+		birthDate.setDay(6);
+		birthDate.setMonth(9);
+		birthDate.setYear(2021);
+
+		System.out.printf("%02d/%02d/%04d%n", birthDate.getDay(), birthDate.getMonth(), birthDate.getYear());
 	}
 }
 
+class Date {
+	private int m_day, m_month, m_year;
 
-class B {
-	public void bar()
-	{
-		A a = new A();
-
-		a.x = 10;
-		a.foo();
-	}
-}
-
-class A {
-	int x;
-	A()
+	public Date(int day, int month, int year)
 	{
 		//...
+		m_day = day;
+		m_month = month;
+		m_year = year;
 	}
 
-	void foo()
+	public int getDay()
+	{
+		return m_day;
+	}
+
+	public void setDay(int day)
 	{
 		//...
+		m_day = day;
 	}
+
+	public int getMonth()
+	{
+		return m_month;
+	}
+
+	public void setMonth(int month)
+	{
+		//...
+		m_month = month;
+	}
+
+	public int getYear()
+	{
+		return m_year;
+	}
+
+	public void setYear(int year)
+	{
+		//...
+		m_year = year;
+	}
+
+	//...
 }
