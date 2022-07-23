0915af6714ea71b1a970065cc48d450565719f4b
Oğuz Karan <oguzkaran@csystem.org>
2022-07-23 10:37:56 +0300

    Update

--- src/Sample/src/org/csystem/app/school/LectureInfoParserTest.java
+++ src/Sample/src/org/csystem/app/school/LectureInfoParserTest.java
@@ -5,7 +5,7 @@
 	Oğuz Karan:Matematik:04/04/2022:01/07/2022:78:67
 	Barış Er:Radyo Haberleşmesi:03/04/2022:02/07/2022:89:100
 	Ziya ÇAYLAN:PLC Programlama:10/04/2022:13/06/2022:90:98
-	Emirhan Kabal:Fizik:07.03.2022:03:07:2022:85:75
+	Emirhan Kabal:Fizik:07/03/2022:03/07/2022:85:75
 	Ozan Yiğit:İstatistik:07/04/2017:19/06/2017:90:100
 	Yunus Emre Uslu:Yazlım Geliştirme:28/04/2022:28/06/2022:60:70
 
